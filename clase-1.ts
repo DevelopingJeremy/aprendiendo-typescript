38df6a5bb0eef922ecf93c53dcd02bc4e1881dc5
DevelopingJeremy <quesadajeremy7@gmail.com>
2026-02-14 14:46:36 -0600

    Espaciaddo e type intersections@

--- clase-1.ts
+++ clase-1.ts
@@ -23,7 +23,14 @@ function saludar3 ({nombre, edad}: {nombre: string, edad: number}): string {
 }
 
 
-// Ahora con funciones o callbacks
+
+
+
+
+
+
+
+//* Ahora con funciones o callbacks
 
 // fn: (name: string) => void
 // Esto significa que fn es una funcion que recibe un string y no retorna nada (void), o mas bien que no nos
@@ -58,6 +65,24 @@ avengers.forEach(avenger => {
     console.log(avenger.toUpperCase()); // infiere que como tiene texto ese [], deja poner metodos de strings 
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //* Objetos
 
 let heroe = {
@@ -70,6 +95,24 @@ function crearHeroe (nombre: string, edad: number) {
 }
 
 const thor = crearHeroe('Thor', 1500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //* Type Alias
 
@@ -112,12 +155,84 @@ const thor3 = Object.freeze(createHero({ nombre: 'Thorcito', edad: 200}))
 thor2.id?.toString()
 
 
+
+
+
+
+
+
+
+
+
+
+
 //* Template Union Types
+// Esto es un tipo de dato que se basa en una plantilla, y solo acepta strings que sigan esa plantilla, por ejemplo, para un UUID
 
 // Imaginemos que guardamos colores, pero no sabemos si lleva # o no.
-
-
 type ColorHexadecimal = `#${string}`; // Esto es un tipo de dato que solo acepta strings que empiecen con #, y el resto puede ser cualquier string
 
 const color1: ColorHexadecimal = '#ff0000'; // Esto es valido
 // const color2: ColorHexadecimal = 'ff0000'; // Esto no es valido porque no empieza con #, nos va a tirar error
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* Union Types
+// Union Types es un tipo de dato que puede ser de varios tipos, por ejemplo, un string o un numero, o incluso un valor especifico como 'loading' o 'success'
+
+// Esto es un tipo de dato que puede ser de varios tipos, en este caso string o number
+type HeroName = string | number;
+
+const name1: HeroName = 'Hulk'; // Esto es valido
+const name2: HeroName = 123; // Esto tambien es valido
+// const name3: HeroName = true; //! Esto no es valido porque no es ni string ni number, nos va a tirar error
+
+
+// Tambien podemos con diferentes valores en vez de tipos de datos, por ejemplo, para un estado de una aplicacion
+type AppState = 'loading' | 'success' | 'error';
+
+let state: AppState = 'loading'; // Esto es valido
+state = 'success'; // Esto tambien es valido
+// state = 'other'; //! Esto no es valido porque no es ninguno de los valores permitidos, nos va a tirar error
+
+
+// Tambien podemos combinar ambos, por ejemplo, para un estado de una aplicacion que puede ser un string o un valor especifico
+type AppState2 = 'loading' | 'success' | 'error' | string;
+
+let state2: AppState2 = 'loading'; // Esto es valido
+state2 = 'success'; // Esto tambien es valido
+state2 = 'other'; // Esto tambien es valido porque ahora si puede ser cualquier string
+// state2 = 123; //! Esto no es valido porque no es ni string ni ninguno de los valores permitidos, nos va a tirar error
+
+
+
+
+
+
+
+
+
+
+
+
+//* Intersection Types
+// Intersection Types es un tipo de dato que combina varios tipos, es decir, un valor que debe cumplir con las propiedades de todos los tipos combinados
+
+// Esto es un tipo de dato que combina varios tipos, en este caso, un objeto que tiene las propiedades de ambos tipos
+type HeroWithPower = Hero & { power: string }; // Esto es un tipo de dato que combina el tipo Hero con un objeto que tiene una propiedad power de tipo string
+
+// Tambien podemos combinar varios tipos o hacer combinaciones
+type HeroFinal = Hero & History & { power: string }
+
+//* Type indexing
+
